41d06f4dd52e1815001a7bae56e7ede1745a4a4f
jiseop121 <ghdwltjq5749@naver.com>
2026-03-12 01:11:13 +0900

    chore(formula): release pbdash v0.5.2

--- Formula/pbdash.rb
+++ Formula/pbdash.rb
@@ -6,10 +6,10 @@ class Pbdash < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/jiseop121/pbdash/releases/download/v0.5.2/pbdash-v0.5.2-darwin-arm64.tar.gz"
-      sha256 "f3bf7b1d469d62e2bf986835ec130bdded0e3d73f8de61e45cb6f105a1726e86"
+      sha256 "19ed6ec26e337b9a06f77292e4b2ecc7170c6a96feba3ce9881ac4e478d950e7"
     else
       url "https://github.com/jiseop121/pbdash/releases/download/v0.5.2/pbdash-v0.5.2-darwin-amd64.tar.gz"
-      sha256 "95603db29aa2498beb3f00ff45746fe6df2fa2dbc8ace8525054cb5e475b72f5"
+      sha256 "f71612df30601ad86c99c75a2d1cf34306fa3856b46a7bd1fc64c15d7b8c0a34"
     end
   end
 
